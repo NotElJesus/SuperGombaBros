c6d76da42d4d6857ae4c8c5d9719a719d89215cb
NotElJesus <chagitodeveloper@gmail.com>
2024-01-26 16:41:36 -0800

    Fixed the Camera Movement.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -6,8 +6,12 @@ public class Script : MonoBehaviour
 {
     public Transform player;
 
+    // Have the camera follow the player but also respect the borders of the game.
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, 0, 0) + new Vector3(0, 0, -10);
+        if (player.transform.position.x > 0)
+        {
+            transform.position = new Vector3(player.transform.position.x, 0, 0) + new Vector3(0, 0, -10);
+        }
     }
 }
