b85ab71b89c9372c771e67beb5a5a013108eb673
Sambhav <68431069+SButterscotch@users.noreply.github.com>
2024-01-27 13:11:28 -0800

    test

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    
+
+    void Start()
+    {
+        startpos = transfrom.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = (cam.transfrom.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + distance, transform.position,y, transform.position.z);
+    }
+}
