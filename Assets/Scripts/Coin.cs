831c151e5f9e6fd8300d420ba26de942c88f89db
Upal113 <upalkundu287@gmail.com>
2024-01-29 07:13:45 -0800

    Added Circle and Coin

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnAndVanishCircles : MonoBehaviour
+{
+    // Define Prefab
+    [SerializeField] public GameObject circlePrefab;
+    public int numberOfCircles = 3;
+    public float minCircleSpawnTime = 0.5f;
+    public float maxCircleSpawnTime = 0.8f;
+
+    public void Start()
+    {
+        // Create and Update circles randomly
+        StartCoroutine(SpawnAndVanishLoop());
+    }
+
+    IEnumerator SpawnAndVanishLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minCircleSpawnTime, maxCircleSpawnTime));
+            // Create number of Circles
+            for (int i = 0; i < numberOfCircles; i++)
+            {
+                SpawnAndVanishCircle();
+            }
+        }
+    }
+    // Create and Delete circles
+    public void SpawnAndVanishCircle()
+    {
+        Vector3 spawnPosition = GetRandomSpawnPosition();
+        GameObject spawnedCircle = Instantiate(circlePrefab, spawnPosition, Quaternion.identity);
+
+        // Set a random lifetime for the spawned circle
+        float lifetime = Random.Range(2f, 6f);
+
+        StartCoroutine(DestroyAfterTime(spawnedCircle, lifetime));
+    }
+    // Get Random Position
+    private Vector3 GetRandomSpawnPosition()
+    {
+        float spawnX = Random.Range(-5f, 5f);
+        float spawnY = Random.Range(-2f, 2f);
+
+        return new Vector3(spawnX, spawnY, 0f);
+    }
+
+    // Destroy Circles
+    IEnumerator DestroyAfterTime(GameObject obj, float lifetime)
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(obj);
+    }
+}
