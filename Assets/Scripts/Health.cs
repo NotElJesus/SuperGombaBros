d754985a8e24e1eef76e136e5cdc90f6c23abc2b
incognitosushiroll <157520631+incognitosushiroll@users.noreply.github.com>
2024-01-29 15:06:50 -0800

    Messing around with FinalBoss death animation - can't turn off loop

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -49,8 +49,8 @@ public class Health : MonoBehaviour
                 animator.runtimeAnimatorController = boss.MushrioDead as RuntimeAnimatorController;
             }
 
-            // Wait for 2.5 seconds before loading the "EndMenu" scene
-            StartCoroutine(LoadGameOverSceneAfterDelay(2.5f));
+            // Wait for 1.5 seconds before loading the "EndMenu" scene
+            StartCoroutine(LoadGameOverSceneAfterDelay(1.5f));
         }
     }
 
@@ -93,66 +93,3 @@ public class Health : MonoBehaviour
             SceneManager.LoadScene("EndMenu"); // Replace "GameOverSceneName" with the actual name of your game over scene
         }
 }
-
-
-
-
-
-
-
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
-
-// public class Health : MonoBehaviour
-// {
-//     public int maxHealth = 150;
-//     public int currentHealth;
-//     public HealthBar healthBar;
-
-    
-//     void Start()
-//     {
-//         currentHealth = maxHealth;
-//         healthBar.SetMaxHealth(maxHealth);
-//     }
-
-//     void Update()
-//     {
-//         // You can handle other player input or events here if needed
-//     }
-
-//     // Call this function to apply damage and handle collisions with obstacles
-//     public void HandleCollisions(Collision2D collision)
-//     {
-//         if (collision.transform.tag == "Obstacle")
-//         {
-//             TakeDamage(10);
-//             Destroy(collision.gameObject); // Destroy the obstacle
-//             // Add any other logic you need, like pausing or handling the jump over the obstacle
-//         }
-//         if (collision.transform.tag == "FinalBoss") { 
-//             if (currentHealth >= 20) { //and coins //kill FinalBoss
-//                 // currentState = MehrioDeadState; 
-//                 // currentState.EnterState(this); 
-//                 //Destroy(collision.gameObject); 
-//             } else { 
-//                 //kill Player 
-//             }
-//         }
-//     }
-
-//     void TakeDamage(int damage)
-//     {
-
-//         if (currentHealth < 20) {
-//             //currentState = EntityDeadState; will have to change state here 
-//             //Destroy(gameObject);
-//             currentHealth = 0;  
-//         }
-//         currentHealth -= damage;
-//         healthBar.SetHealth(currentHealth);
-//     }
-// }
-
