5b9de7854165813483c70c16c33f01538173a9c3
Upal113 <upalkundu287@gmail.com>
2024-02-01 00:11:51 -0800

    Added HUD

--- Assets/Scripts/PlayerHitsCoin.cs
+++ Assets/Scripts/PlayerHitsCoin.cs
@@ -6,14 +6,13 @@ public class PlayerHitsCoin : MonoBehaviour
 {
     public CoinHealthBar coinbar;
     private int coins = 0;
-
     public void OnCollisionEnter2D(Collision2D other)
     {
         if(other.transform.tag == "Coin")
         {
             coins+= 10;
             Debug.Log("Coin Hit");
-            coinbar.SetHealth(coins);
+            coinbar.UpdateHealth(coins);
             Destroy(other.gameObject);
         }
     }
