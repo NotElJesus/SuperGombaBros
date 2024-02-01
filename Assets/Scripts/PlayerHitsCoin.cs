d638591d2346c620863a050868e43446b0616ddd
Matt Kurtz <101678272+matt-kurtz@users.noreply.github.com>
2024-01-31 18:19:25 -0800

    Coin sound effects added

--- Assets/Scripts/PlayerHitsCoin.cs
+++ Assets/Scripts/PlayerHitsCoin.cs
@@ -7,10 +7,13 @@ public class PlayerHitsCoin : MonoBehaviour
     public CoinHealthBar coinbar;
     private int coins = 0;
 
+    [SerializeField] private AudioSource audioSource; // 1st step
+
     public void OnCollisionEnter2D(Collision2D other)
     {
         if(other.transform.tag == "Coin")
         {
+            audioSource.Play(); // 2nd step
             coins+= 10;
             Debug.Log("Coin Hit");
             coinbar.SetHealth(coins);
