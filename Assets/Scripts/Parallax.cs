c35a62f9373d6147e269d409cfca933adc31700a
Sam <68431069+SButterscotch@users.noreply.github.com>
2024-02-22 10:27:59 -0800

    infinite

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -18,7 +18,14 @@ public class Parallax : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //infinite 
+        float temp = (cam.transform.position.x * (1-parallaxEffect));
+
         float distance = (cam.transform.position.x * parallaxEffect);
         transform.position = new Vector3(startpos+distance, transform.position.y, transform.position.z);
+
+        //infinite 
+        if(temp > startpos + length) startpos += length;
+        else if(temp < startpos - length) startpos -= length;
     }
 }
